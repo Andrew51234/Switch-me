75f7f445767be66ae38506eb2c1e2a720eb8e745
Andrew Hany <andrewdodz51234@gmail.com>
2021-10-11 23:21:20 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className={style.root}>
-        <Login />
+        Testing react
       </div>
     </ThemeProvider>
   );
