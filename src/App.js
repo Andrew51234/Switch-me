6bfba6b6b8e07ea521d4de7729c63991f9376aaa
Andrew Hany <andrewdodz51234@gmail.com>
2021-10-11 23:31:05 +0200

    reverting

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className={style.root}>
-        React gamed gedan
+        <Login />
       </div>
     </ThemeProvider>
   );
